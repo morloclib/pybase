2eebd869f25f4381119bb4375c6dd57716335b34
Zebulun Arendsee <arendsee@iastate.edu>
2019-04-20 07:29:10 -0500

    Add support for DataFrame serialization

--- pybase.py
+++ pybase.py
@@ -1,4 +1,12 @@
 import json
+import pandas as pd
+import numpy as np
 
 unpackGeneric = json.loads
 packGeneric = json.dumps
+
+def unpackDataFrame(df):
+    return(df.to_json())
+
+def packDataFrame(json):
+    return(pd.read_json(json))
