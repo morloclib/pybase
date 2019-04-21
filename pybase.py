102ff0cca3def759d4d7e494ca9ece38ade341b4
Zebulun Arendsee <arendsee@iastate.edu>
2019-04-21 13:33:43 -0500

    Pack DataFrame as table with schema

--- pybase.py
+++ pybase.py
@@ -6,7 +6,23 @@ unpackGeneric = json.loads
 packGeneric = json.dumps
 
 def unpackDataFrame(df):
-    return(df.to_json())
+    return(df.to_json(orient="table"))
+#### example format for a character count table
+#  {
+#      "schema": {
+#          "fields":[
+#              {"name":"index","type":"string"},
+#              {"name":"count","type":"integer"}
+#          ],
+#          "primaryKey":["index"],
+#          "pandas_version":"0.20.0"
+#      },
+#      "data": [
+#          {"index":"f","count":2},
+#          {"index":"d","count":2},
+#          {"index":"s","count":1}
+#      ]
+#  }
 
 def packDataFrame(json):
     return(pd.read_json(json))
