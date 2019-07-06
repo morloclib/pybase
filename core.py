8919ca9f2a1582bc1f1ac2d73152f691a5b15601
Zebulun Arendsee <arendsee@iastate.edu>
2019-07-06 13:49:06 -0500

    Add arithmetic

--- core.py
+++ core.py
@@ -6,3 +6,15 @@ def fold(f, b, xs):
   for x in xs:
     b = f(b, x)
   return b
+
+def add(a,b):
+  return a+b
+
+def sub(a,b):
+  return a-b
+
+def mul(a,b):
+  return a*b
+
+def div(a,b):
+  return a/b
