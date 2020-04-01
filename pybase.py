09bf482be1d5daed5466ba4698b479f8f11efc84
Zebulun Arendsee <arendsee@iastate.edu>
2020-04-01 12:24:52 -0500

    Remove numpy and pandas dependencies
    The base python should have no heavy dependencies.
    
    I really need to find a good system for including in the morloc builds
    only the dependencies that are used.

--- pybase.py
+++ pybase.py
@@ -1,6 +1,6 @@
 import json
-import pandas as pd
-import numpy as np
+#  import pandas as pd
+#  import numpy as np
 
 unpackGeneric = json.loads
 packGeneric = json.dumps
@@ -15,32 +15,32 @@ def unpackList(x):
 packFloatList = packList
 unpackFloatList = unpackList
 
-def packDataFrame(df):
-    return(df.to_json(orient="table"))
-#### example format for a character count table
-#  {
-#      "schema": {
-#          "fields":[
-#              {"name":"index","type":"string"},
-#              {"name":"count","type":"integer"}
-#          ],
-#          "primaryKey":["index"],
-#          "pandas_version":"0.20.0"
-#      },
-#      "data": [
-#          {"index":"f","count":2},
-#          {"index":"d","count":2},
-#          {"index":"s","count":1}
-#      ]
-#  }
-
-def unpackDataFrame(json):
-    return(pd.read_json(json))
-
-#  JSON -> Matrix
-def unpackMatrix(x):
-    return(np.matrix(json.loads(x)))
-
-#  Matrix -> JSON
-def packMatrix(x):
-    return(json.dumps(x.tolist()))
+#  def packDataFrame(df):
+#      return(df.to_json(orient="table"))
+#  #### example format for a character count table
+#  #  {
+#  #      "schema": {
+#  #          "fields":[
+#  #              {"name":"index","type":"string"},
+#  #              {"name":"count","type":"integer"}
+#  #          ],
+#  #          "primaryKey":["index"],
+#  #          "pandas_version":"0.20.0"
+#  #      },
+#  #      "data": [
+#  #          {"index":"f","count":2},
+#  #          {"index":"d","count":2},
+#  #          {"index":"s","count":1}
+#  #      ]
+#  #  }
+#
+#  def unpackDataFrame(json):
+#      return(pd.read_json(json))
+#
+#  #  JSON -> Matrix
+#  def unpackMatrix(x):
+#      return(np.matrix(json.loads(x)))
+#
+#  #  Matrix -> JSON
+#  def packMatrix(x):
+#      return(json.dumps(x.tolist()))
