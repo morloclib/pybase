bdc277c161a2c373bc43c06060e867aa0233cb88
Zebulun Arendsee <zbwrnz@gmail.com>
2023-03-26 18:05:51 -0500

    Partial sync with case study

--- core.py
+++ core.py
@@ -1,3 +1,9 @@
+# imports a bit of a problem, the incur a startup cost even if they are not
+# used. I need to find a way to minimize this by making them load lazily. Or
+# perhaps by "shaking the tree" and/or compiling the code.
+import time
+import copy
+
 def mlc_neg(x):
   return (-1) * x
 
@@ -31,8 +37,48 @@ def mlc_div(a,b):
 def mlc_map(f, *args):
   return list(map(f, *args))
 
+def mlc_filter(f, xs):
+    return [x for x in xs if f(x)]
+
 def mlc_fst(x):
   return x[0]
 
 def mlc_snd(x):
   return x[0]
+
+#  onFst :: (a -> a') -> (a, b) -> (a', b)
+def mlc_onFst(f, x):
+    return(f(x[0]))
+
+#  concat :: [[a]] -> [a]
+def mlc_concat(xss):
+    ys = []
+    for xs in xss: 
+        ys.append(xs)
+    return ys
+
+#  sleep py :: Real -> a -> a
+def mlc_sleep(n, a):
+    time.sleep(n)
+    return a
+
+#  shard py :: Int -> [a] -> [[a]]
+def mlc_shard(chunkSize, xs):
+    if(len(xs) == 0):
+        return [[]]
+    else:
+        xss = []
+        for i in range(1, len(xs) + 1):
+            if i % chunkSize == 0:
+                xss.append(xs[i - chunkSize: i])
+        size = chunkSize * (len(xs) // chunkSize)
+        if size < len(xs):
+            xss.append(xs[size - 1: len(xs)])
+    return xss
+
+#  join py :: [a] -> [a] -> [a]
+def mlc_join(xs, ys):
+    # this function should not mutate the data
+    xsCopy = copy.copy(xs)
+    xsCopy.append(ys)
+    return xsCopy
